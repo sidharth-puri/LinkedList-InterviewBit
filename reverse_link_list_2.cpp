01db71667684a5c07ff44dc0803aac4715bbc003
Sidharth Puri <52946626+sidharth-puri@users.noreply.github.com>
2020-02-20 21:15:55 +0530

    Create reverse_link_list_2.cpp

---
+++ reverse_link_list_2.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+ListNode* Solution::reverseBetween(ListNode* A, int B, int C) {
+    ListNode* current=A;
+    int count=1;
+    while(current && count<B-1){
+        current=current->next;
+        count++;
+    }
+    
+    ListNode* revcurr=current->next;
+    current->next=NULL;
+    ListNode* next=NULL;
+    ListNode* prev=NULL;
+    int size=C-B;
+    while(revcurr && size>=0){
+        next=revcurr->next;
+        revcurr->next=prev;
+        prev=revcurr;
+        revcurr=next;
+        size--;
+    }
+   current->next=prev;
+    while(prev->next){
+        prev=prev->next;
+    }
+    prev->next=revcurr;
+    return A;
+}
